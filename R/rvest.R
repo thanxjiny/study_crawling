9d50318699c946184621bd2312583688868394c9
thanxjiny <thanxjiny@gmail.com>
2020-07-23 08:33:03 +0900

    read_html

--- R/rvest.R
+++ R/rvest.R
@@ -1,1 +1,11 @@
  #### rvest ####
+ 
+ # 2020.07.22 study start
+ 
+ library(rvest)
+ 
+ url <- "http://biz.newdaily.co.kr/site/data/html/2020/07/22/2020072200139.html"
+
+ read_html(url)
+ 
+ read_html(url) %>% html_nodes("section h1") %>% html_text
