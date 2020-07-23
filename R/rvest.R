a18ba7f74c9ef53b0f33980c6134d5a5be099671
thanxjiny <thanxjiny@gmail.com>
2020-07-24 08:42:06 +0900

    httr

--- R/rvest.R
+++ R/rvest.R
@@ -1,11 +1,25 @@
- #### rvest ####
- 
- # 2020.07.22 study start
- 
+ #### rvest ####  # 2020.07.22 study start
+
  library(rvest)
  
  url <- "http://biz.newdaily.co.kr/site/data/html/2020/07/22/2020072200139.html"
 
  read_html(url)
  
- read_html(url) %>% html_nodes("section h1") %>% html_text
+ read_html(url) %>% html_nodes("section h1") %>% html_text()
+ 
+ read_html(url) %>% html_nodes("section h1") %>% html_attr("class")
+ 
+ #### httr ####
+ 
+ library(httr)
+ library(dpp )
+ 
+ 
+ GET(url)
+ 
+ 
+ read_html(url) == content(GET(url))
+ 
+
+
